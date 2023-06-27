13e49f53c67e95e2df78a20bb7340d52d2c8eb0c
Fabio Junior <91484736+Fabio-jr-SM@users.noreply.github.com>
2023-06-27 12:29:10 -0300

    Update readability.c

--- Modulo-2-array/exercicios/readability.c
+++ Modulo-2-array/exercicios/readability.c
@@ -1,4 +1,79 @@
-/*#include <stdio.h>
+#include <stdio.h>
+#include <cs50.h>
+#include <string.h>
+#include <ctype.h>
+#include <math.h>
+
+
+int main(void) {
+   
+   string text = get_string("Texto: ");
+   int letras=0, palavras=1,frases=0;
+   
+   for(int i=0; i<strlen(text);i++){
+       if(isalpha(text[i])){
+            letras++;
+       }
+       
+       if(text[i]==' '){
+          palavras++;
+       }
+       
+       if(text[i]=='!' || text[i]=='.'){
+          frases++;
+       }
+   }
+   printf("Letras: %i\nPalavras: %i\nFrases: %i\n",letras,palavras,frases);
+   
+   float L = (letras/palavras)*100;
+   float S = (frases/palavras)*100;
+   
+   float indice = (0.0588*L) - (0.296*S) - 15.8;
+   int grade = (int) round(indice);
+   
+   printf("Grade: %i\n",grade);
+}
+
+
+/***********************************************************
+
+#include <stdio.h>
+#include <cs50.h>
+#include <string.h>
+#include <ctype.h>
+
+int main(void) {
+   
+   string text = get_string("Texto: ");
+   int letras=0, palavras=1,frases=0;
+   
+   for(int i=0; i<strlen(text);i++){
+       if(isalpha(text[i])){
+            letras++;
+       }
+       
+       if(text[i]==' '){
+          palavras++;
+       }
+       
+       if(text[i]=='!' || text[i]=='.'){
+          frases++;
+       }
+   }
+   printf("Letras: %i\nPalavras: %i\nFrases: %i\n",letras,palavras,frases);
+   
+   int L = (letras/palavras)*100;
+   int S = (frases/palavras)*100;
+   
+   int indice = 0.0588 * L - 0.296 * S - 15.8;
+   
+   printf("Grade: %i\n",indice);
+}
+
+
+*************************************************************
+
+#include <stdio.h>
 #include <cs50.h>
 #include <string.h>
 #include <ctype.h>
